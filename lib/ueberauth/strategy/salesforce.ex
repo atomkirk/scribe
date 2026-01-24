e3b0fe1cceb542409a56353804fab987ac5c5cdb
Kamasu Paul <kamasupaul@gmail.com>
2026-01-24 09:17:57 +0300

    add Salesforce authentication strategy and OAuth module

---
+++ lib/ueberauth/strategy/salesforce.ex
@@ -0,0 +1,133 @@
+defmodule Ueberauth.Strategy.Salesforce do
+  @moduledoc """
+  Salesforce Strategy for Ueberauth.
+  """
+
+  use Ueberauth.Strategy,
+    uid_field: :id,
+    default_scope: "api refresh_token full web",
+    oauth2_module: Ueberauth.Strategy.Salesforce.OAuth
+
+  alias Ueberauth.Auth.Info
+  alias Ueberauth.Auth.Credentials
+  alias Ueberauth.Auth.Extra
+
+  @doc """
+  Handles initial request for Salesforce authentication.
+  """
+  def handle_request!(conn) do
+    scopes = conn.params["scope"] || option(conn, :default_scope)
+
+    opts =
+      [scope: scopes, redirect_uri: callback_url(conn)]
+      |> with_optional(:prompt, conn)
+      |> with_param(:prompt, conn)
+      |> with_state_param(conn)
+
+    redirect!(conn, Ueberauth.Strategy.Salesforce.OAuth.authorize_url!(opts))
+  end
+
+  @doc """
+  Handles the callback from Salesforce.
+  """
+  def handle_callback!(%Plug.Conn{params: %{"code" => code}} = conn) do
+    opts = [redirect_uri: callback_url(conn)]
+
+    case Ueberauth.Strategy.Salesforce.OAuth.get_access_token([code: code], opts) do
+      {:ok, token} ->
+        fetch_user(conn, token)
+
+      {:error, {error_code, error_description}} ->
+        set_errors!(conn, [error(error_code, error_description)])
+    end
+  end
+
+  def handle_callback!(conn) do
+    set_errors!(conn, [error("missing_code", "No code received")])
+  end
+
+  @doc """
+  Cleans up the private area of the connection used for passing the raw Salesforce response around during the callback.
+  """
+  def handle_cleanup!(conn) do
+    conn
+    |> put_private(:salesforce_token, nil)
+    |> put_private(:salesforce_user, nil)
+  end
+
+  @doc """
+  Fetches the uid field from the response.
+  """
+  def uid(conn) do
+    uid_field =
+      conn
+      |> option(:uid_field)
+      |> to_string()
+
+    conn.private.salesforce_user[uid_field]
+  end
+
+  @doc """
+  Includes the credentials from the Salesforce response.
+  """
+  def credentials(conn) do
+    token = conn.private.salesforce_token
+
+    %Credentials{
+      expires: true,
+      expires_at: token.expires_at,
+      scopes: String.split(token.other_params["scope"] || "", " "),
+      token: token.access_token,
+      refresh_token: token.refresh_token,
+      token_type: token.token_type
+    }
+  end
+
+  @doc """
+  Fetches the fields to populate the info section of the `Ueberauth.Auth` struct.
+  """
+  def info(conn) do
+    user = conn.private.salesforce_user
+
+    %Info{
+      email: user["email"],
+      name: user["display_name"]
+    }
+  end
+
+  @doc """
+  Stores the raw information obtained from the Salesforce callback.
+  """
+  def extra(conn) do
+    %Extra{
+      raw_info: %{
+        token: conn.private.salesforce_token,
+        user: conn.private.salesforce_user
+      }
+    }
+  end
+
+  defp fetch_user(conn, token) do
+    conn = put_private(conn, :salesforce_token, token)
+
+    case Ueberauth.Strategy.Salesforce.OAuth.get_token_info(token.access_token, token.other_params["instance_url"]) do
+      {:ok, user} ->
+        put_private(conn, :salesforce_user, user)
+
+      {:error, reason} ->
+        set_errors!(conn, [error("token_info_error", reason)])
+    end
+  end
+
+  defp with_param(opts, key, conn) do
+    if value = conn.params[to_string(key)], do: Keyword.put(opts, key, value), else: opts
+  end
+
+  defp with_optional(opts, key, conn) do
+    if option(conn, key), do: Keyword.put(opts, key, option(conn, key)), else: opts
+  end
+
+  defp option(conn, key) do
+    Keyword.get(options(conn), key, Keyword.get(default_options(), key))
+  end
+end
---
+++ lib/ueberauth/strategy/salesforce/oauth.ex
@@ -0,0 +1,129 @@
+defmodule Ueberauth.Strategy.Salesforce.OAuth do
+  @moduledoc """
+  OAuth2 for Salesforce.
+
+  Add `client_id`, `client_secret`, `redirect_uri`, and `instance_url` to your configuration:
+
+      config :ueberauth, Ueberauth.Strategy.Salesforce.OAuth,
+        client_id: System.get_env("SALESFORCE_CLIENT_ID"),
+        client_secret: System.get_env("SALESFORCE_CLIENT_SECRET"),
+        redirect_uri: System.get_env("SALESFORCE_REDIRECT_URI"),
+        instance_url: System.get_env("SALESFORCE_INSTANCE_URL")
+  """
+
+  use OAuth2.Strategy
+
+  @defaults [
+    strategy: __MODULE__,
+    authorize_url: "/services/oauth2/authorize",
+    token_url: "/services/oauth2/token"
+  ]
+
+  @doc """
+  Construct a client for requests to Salesforce.
+
+  This will be setup automatically for you in `Ueberauth.Strategy.Salesforce`.
+
+  These options are only useful for usage outside the normal callback phase of Ueberauth.
+  """
+  def client(opts \\ []) do
+    config = Application.get_env(:ueberauth, __MODULE__, [])
+
+      IO.inspect(config, label: "Salesforce OAuth Config")
+
+    instance_url = config[:instance_url] || "https://orgfarm-d043c8acd2-dev-ed.develop.my.salesforce-setup.com"
+
+    opts =
+      @defaults
+      |> Keyword.merge([site: instance_url])
+      |> Keyword.merge(config)
+      |> Keyword.merge(opts)
+
+    json_library = Ueberauth.json_library()
+
+    OAuth2.Client.new(opts)
+    |> OAuth2.Client.put_serializer("application/json", json_library)
+  end
+
+  @doc """
+  Provides the authorize url for the request phase of Ueberauth.
+  """
+  def authorize_url!(params \\ [], opts \\ []) do
+    opts
+    |> client()
+    |> OAuth2.Client.authorize_url!(params)
+  end
+
+  @doc """
+  Fetches an access token from the Salesforce token endpoint.
+  """
+  def get_access_token(params \\ [], opts \\ []) do
+    config = Application.get_env(:ueberauth, __MODULE__, [])
+    instance_url = config[:instance_url] || "https://login.salesforce.com"
+
+    # Salesforce requires client_id and client_secret in the body
+    params =
+      params
+      |> Keyword.put(:client_id, config[:client_id])
+      |> Keyword.put(:client_secret, config[:client_secret])
+
+    case opts |> client() |> OAuth2.Client.get_token(params) do
+      {:ok, %OAuth2.Client{token: %OAuth2.AccessToken{} = token}} ->
+        # Preserve instance_url in other_params for later use
+        token = %{token | other_params: Map.put(token.other_params, "instance_url", instance_url)}
+        {:ok, token}
+
+      {:ok, %OAuth2.Client{token: nil}} ->
+        {:error, {"no_token", "No token returned from Salesforce"}}
+
+      {:error, %OAuth2.Response{body: %{"error" => error, "error_description" => description}}} ->
+        {:error, {error, description}}
+
+      {:error, %OAuth2.Response{body: %{"message" => message, "status" => status}}} ->
+        {:error, {status, message}}
+
+      {:error, %OAuth2.Error{reason: reason}} ->
+        {:error, {"oauth2_error", to_string(reason)}}
+    end
+  end
+
+  @doc """
+  Fetches user info from Salesforce to get user_id and email.
+  """
+  def get_token_info(access_token, instance_url) do
+    url = "#{instance_url}/services/oauth2/userinfo"
+
+    case Tesla.get(http_client(access_token), url) do
+      {:ok, %Tesla.Env{status: 200, body: body}} ->
+        {:ok, body}
+
+      {:ok, %Tesla.Env{status: status, body: body}} ->
+        {:error, "Failed to get token info: #{status} - #{inspect(body)}"}
+
+      {:error, reason} ->
+        {:error, "HTTP error: #{inspect(reason)}"}
+    end
+  end
+
+  defp http_client(access_token) do
+    Tesla.client([
+      {Tesla.Middleware.Headers, [{"authorization", "Bearer #{access_token}"}]},
+      Tesla.Middleware.JSON
+    ])
+  end
+
+  # OAuth2.Strategy callbacks
+
+  @impl OAuth2.Strategy
+  def authorize_url(client, params) do
+    OAuth2.Strategy.AuthCode.authorize_url(client, params)
+  end
+
+  @impl OAuth2.Strategy
+  def get_token(client, params, headers) do
+    client
+    |> put_param(:grant_type, "authorization_code")
+    |> put_header("Content-Type", "application/x-www-form-urlencoded")
+    |> OAuth2.Strategy.AuthCode.get_token(params, headers)
+  end
+end
